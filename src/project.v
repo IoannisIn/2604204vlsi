9252e39ab185cefacdd65954564edf8968ad2f16
IoannisIn <intzes.ioannis@gmail.com>
2024-04-26 07:27:35 +0300

    Update project.v

--- src/project.v
+++ src/project.v
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-//`default_nettype none
+`default_nettype none
 
 module tt_um_ioannisin_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
@@ -16,22 +16,23 @@ module tt_um_ioannisin_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-    wire ui_in[0] = a;
-    wire ui_in[1] = b;
-    wire ui_in[2] = c;
-    wire ui_in[3] = d;
-    wire ui_in[4] = s[0];
-    wire ui_in[5] = s[1];
-    wire ui_in[6] = e;
-    wire ui_in[7] = f;
+    // Assigning dedicated inputs to internal signals
+    assign ui_in[0] = a;
+    assign ui_in[1] = b;
+    assign ui_in[2] = c;
+    assign ui_in[3] = d;
+    assign ui_in[4] = s[0];
+    assign ui_in[5] = s[1];
+    assign ui_in[6] = e;
+    assign ui_in[7] = f;
 
-    assign uo_out[0] = y;
-    assign uo_out[7:1] = 7'b0;
+    // Assigning dedicated outputs
+    assign y = uo_out[0];
+    assign uo_out[7:1] = 7'b0;  // Setting other bits to 0
     
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out [7:1] = 7'b0;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-    
+    // All output pins must be assigned. If not used, assign to 0.
+    assign uio_out = 8'b0; // Example: uio_out is not used, so assigning it to 0
+    assign uio_oe  = 8'b0; // Example: uio_oe is not used, so assigning it to 0
+
 
 endmodule
