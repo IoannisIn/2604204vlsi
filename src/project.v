8ac187512ded0a10332a61834f3b2cfd76e82886
IoannisIn <intzes.ioannis@gmail.com>
2024-04-26 07:20:21 +0300

    Update project.v

--- src/project.v
+++ src/project.v
@@ -29,7 +29,7 @@ module tt_um_ioannisin_example (
     assign uo_out[7:1] = 7'b0;
     
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out [7:1] = 7'b0;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
     
